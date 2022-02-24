e1a15b57c0799b5e720ffc491aadf547343e957d
JianB <Beerensjian@gmail.com>
2022-02-24 12:37:45 +0100

    Case 2: first solution

---
+++ CASE_2/Case_2.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function pre_r($array)
+{
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+
+class Item
+{
+    public string $itemName;
+    public float $price;
+    public int $quantity;
+    public float $taxRate;
+    public string $type;
+
+    public function __construct(string $itemName, float $price, int $quantity, float $taxRate, string $type)
+    {
+        $this -> itemName = $itemName;
+        $this -> price = $price;
+        $this -> quantity = $quantity;
+        $this -> taxRate = $taxRate;
+        $this -> type = $type;
+    }
+
+    function getPrice()
+    {
+        return $this -> price * $this -> quantity;
+    }
+    function getTax()
+    {
+        return $this->getPrice() * $this -> taxRate;
+    }
+    // New Case 2 Function
+    function setDiscount(float $percentage)
+    {
+        $this -> price = $this -> price * $percentage;
+    }
+
+}
+
+$bananas = new Item("Banaan", 1, 6, 0.06, "fruit");
+$apples = new Item("Apple", 1.5, 3, 0.06, "fruit");
+$wineBottles = new Item("Bottle Of Wine", 10, 2, 0.21, "wine");
+
+$totalTax = 0;
+echo $totalTax = $bananas ->getTax() + $apples -> getTax() + $wineBottles -> getTax();
+echo "<br>";
+$totalPrice = 0;
+echo $totalPrice = $bananas -> getPrice() + $apples -> getPrice() + $wineBottles ->getPrice();
+
+// Case 2 modifications start here
+
+$bananas -> setDiscount(0.50);
+$apples -> setDiscount(0.50);
+echo "<br>" . $bananas -> price . "<br>" . $apples -> price;
+
+
+
